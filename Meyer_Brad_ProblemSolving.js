fca09946123cd91ffcf3a9c9426f58f576efeb67
Wbradmeyer <107085402+Wbradmeyer@users.noreply.github.com>
2023-06-24 22:05:47 -0400

    Add files via upload

---
+++ Meyer_Brad_ProblemSolving.js
@@ -0,0 +1,23 @@
+1.
+// We need to decide if a paragraph is in First, Second, or Third person
+// We could check all the words of the paragraph and count I, We, You, He, She, They, etc.
+// whichever count is the highest indicates the person
+
+2. 
+// declare three arrays with subjects for first, second, or third person
+// declare three counts for first, second, or third person and start them at zero
+// write a function that takes an argument 'paragraph'
+// change the paragraph into an array of all the words
+
+// use two for loops to check each word against the first person array of subjects
+// increment the first person count for every paragraph word that matches a first person subject
+// repeat the two previous steps for the second and third array/counts
+
+// compare the count values to find which is the largest
+// return a string that indicates if the paragraph is in first, second, or third person
+
+3. 
+/* The hardest part about problem-solving is considering the whole problem up front. I tend to want
+to start trying things right away. It is better to begin by stating the goal and then beginning to come up 
+with a strategy to try. This requires the discipline to stop and lay out the steps that will
+help achieve the goal.*/
